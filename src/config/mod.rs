acb7bced5827005bc6294d8b3c3a876d3f845d44
Jeremy Soller <jeremy@system76.com>
2019-06-10 17:31:44 -0600

    0.2.4 - allow empty configuration sections and update dependencies

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,7 +8,10 @@ mod user;
 #[derive(Debug, Default, Deserialize)]
 pub struct Config {
     pub general: general::GeneralConfig,
+    #[serde(default)]
     pub packages: BTreeMap<String, package::PackageConfig>,
+    #[serde(default)]
     pub files: Vec<file::FileConfig>,
+    #[serde(default)]
     pub users: BTreeMap<String, user::UserConfig>,
 }
