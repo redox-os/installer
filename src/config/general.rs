adfee954840aa84b1e1a5605f0b5cf727bed324e
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-01-04 15:08:49 +0100

    Support including other config files from a config file
    This will allow significantly reducing the amount of duplication
    between all the different configs used by Redox OS.

--- src/config/general.rs
+++ src/config/general.rs
@@ -5,3 +5,11 @@ pub struct GeneralConfig {
     pub repo_binary: Option<bool>,
     pub efi_partition_size: Option<u32>, //MiB
 }
+
+impl GeneralConfig {
+    pub(super) fn merge(&mut self, other: GeneralConfig) {
+        self.prompt = other.prompt;
+        self.repo_binary = other.repo_binary.or(self.repo_binary);
+        self.efi_partition_size = other.efi_partition_size.or(self.efi_partition_size);
+    }
+}
